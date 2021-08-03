0cb28b211718906c291e4d50e4b05dd61a4bc7dc
Adam Herro <adam.herro@comcast.net>
2021-08-03 18:26:51 -0400

    Starting to add functionality and separating data classes from CSS classes

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const number = document.querySelector('.num')
+const numberBtns = document.querySelectorAll('[data-number]')
+const clear = document.querySelector('[data-clear]');
+const operations = document.querySelectorAll('[data-operation]');
+const deleteBtn = document.querySelector('[data-delete]');
+const equals = document.querySelector('[data-equals]');
+
+
+//Display value of button in display section
+let firstOperand = numberBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        number.textContent += btn.textContent
+        return number.textContent;
+        
+    })
+})
+
+//Operation Functions
 const add = (a, b) => {
     return a + b;
 }
@@ -11,10 +29,33 @@ const divide = (a, b) => {
     return a / b;
 }
 
+//Clear Function
+clear.addEventListener('click', () => {
+    number.textContent = '';
+})
+
+
+//Add button
+addition.addEventListener('click', add)
+
+//Subtract button
+subtraction.addEventListener('click', subtract);
+
+//Multiply button
+multiplication.addEventListener('click', multiply);
+
+//Division button 
+division.addEventListener('click', divide);
+
+
+
+//Operate function
 const operate = (opr, num1, num2) => {
-    opr.match(/[+-*/]/)
+    /*
+    opr.match(/[+-/*]/)
     num1.match(/[0-9]/g)
     num1.match(/[0-9]/g)
+    */
     if (opr === '+') {
         return add(num1, num2)
     } else if (opr === '-') {
@@ -24,5 +65,5 @@ const operate = (opr, num1, num2) => {
     } else if (opr === '/') {
         return divide(num1, num2)
     }
-
 }
+operate(firstOperand, secondOperand)
