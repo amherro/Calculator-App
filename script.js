cd26c9d9494fa73c50747e76f47594425a88cacc
Adam Herro <adam.herro@comcast.net>
2021-08-04 17:16:43 -0400

    Getting first input and operation to display on calculator and updating clear function.

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-const currentOperandText = document.querySelector('[data-current-operand]');
-const previousOperandText = document.querySelector('[data-previous-operand]')
+//Need to figure out how to display second input and make compute function work 
+
+let currentOperandText = document.querySelector('[data-current-operand]');
+let currentOperation = document.querySelector('[data-current-operation]')
+let previousOperandText = document.querySelector('[data-previous-operand]')
 const numberBtns = document.querySelectorAll('[data-number]');
 const clear = document.querySelector('[data-clear]');
 const operations = document.querySelectorAll('[data-operation]');
 const deleteBtn = document.querySelector('[data-delete]');
 const equals = document.querySelector('[data-equals]');
 
-
+let updateDisplay = () => {
+    previousOperandText.textContent  = currentOperandText.textContent
+    
+}
 //Display value of button in display section
 numberBtns.forEach((number) => {
     number.addEventListener('click', () => {
-        currentOperandText.textContent += number.textContent
-        return currentOperandText;
-    })
+        currentOperandText.textContent += number.textContent;
+    })  
 })
 
+//Display Operation on Calculator
+operations.forEach((opr) => {
+    opr.addEventListener('click', () => {
+        updateDisplay();
+        currentOperation.textContent = opr.textContent; 
+        currentOperandText.textContent = '';       
+    })            
+})
+
+
+//Clear Function 
+clear.addEventListener('click', () => {
+    currentOperandText.textContent = '';
+    currentOperation.textContent = '';
+    previousOperandText.textContent = '';
+})
+
+function compute() {
+    if (this.currentOperation === '+') {
+        return previousOperandText - currentOperandText;
+    } else if (this.currentOperation === '-') {
+        return previousOperandText - currentOperandText;
+    } else if (this.currentOperation === '*') {
+        return previousOperandText - currentOperandText;
+    } else if (this.currentOperation === '/') {
+        return previousOperandText - currentOperandText;
+    };
+}
+
+equals.addEventListener('click', () => {
+    compute();
+});
+
+/*
 //Operation Functions
 const add = (a, b) => {
     return a + b;
@@ -29,22 +68,11 @@ const divide = (a, b) => {
     return a / b;
 }
 
-//Clear Function
-clear.addEventListener('click', () => {
-    currentOperandText.textContent = '';
-})
-
-
 
 //Operate function
 const operate = (opr, num1, num2) => {
-    /*
-    opr.match(/[+-/*]/)
-    num1.match(/[0-9]/g)
-    num1.match(/[0-9]/g)
-    */
-    if (opr === '+') {
-        return add(num1, num2)
+    if (this.opr === '+') {
+        currentOperandText.textContent = add(num1, num2)
     } else if (opr === '-') {
         return subtract(num1, num2)
     } else if (opr === '*') {
@@ -53,3 +81,6 @@ const operate = (opr, num1, num2) => {
         return divide(num1, num2)
     }
 }
+
+operate(currentOperation, previousOperandText, currentOperandText);
+*/
